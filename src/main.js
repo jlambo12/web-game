e3f0acdaa02024936fc90aecbdf5bd04a03677c1
jlambo12 <37668016+jlambo12@users.noreply.github.com>
2026-04-10 07:18:48 +0500

    Add app entry point

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import "./styles/variables.css";
+import "./styles/globals.css";
+import "./styles/layout.css";
+import "./styles/hud.css";
+
+import { bootstrapApp } from "./app/bootstrap.js";
+
+bootstrapApp();
