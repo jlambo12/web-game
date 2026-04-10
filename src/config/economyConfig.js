f1a97a908d2c0004544bc908c82c33b43e72bb74
jlambo12 <37668016+jlambo12@users.noreply.github.com>
2026-04-10 07:21:16 +0500

    Add economy config scaffold

---
+++ src/config/economyConfig.js
@@ -0,0 +1,9 @@
+export const ECONOMY_CONFIG = {
+  shardsPerLevelUp: 1,
+  foodMilestoneEvery: 5,
+  shardsPerFoodMilestone: 1,
+  scoreRewardThresholds: [150, 350, 700, 1200],
+  scoreRewardValues: [1, 2, 3, 4],
+  newRecordRewardBase: 3,
+  runValueMultiplier: 0.01,
+};
