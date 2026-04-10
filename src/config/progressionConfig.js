15cf4311e6796aee5b5ee7e03a12a687040d30db
jlambo12 <37668016+jlambo12@users.noreply.github.com>
2026-04-10 07:20:59 +0500

    Add progression config scaffold

---
+++ src/config/progressionConfig.js
@@ -0,0 +1,10 @@
+export const PROGRESSION_CONFIG = {
+  baseGridSize: 20,
+  baseStartLength: 4,
+  fieldExpansionStartLevel: 3,
+  fieldExpansionStep: 3,
+  maxFieldExpansions: 5,
+  baseXpToNext: 5,
+  xpGrowthFactor: 1.35,
+  scorePerFood: 10,
+};
