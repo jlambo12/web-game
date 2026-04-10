c4a255667dc5e4dab833a2c9a2efcdb5611f96a8
jlambo12 <37668016+jlambo12@users.noreply.github.com>
2026-04-10 07:19:53 +0500

    Add base Phaser config

---
+++ src/config/gameConfig.js
@@ -0,0 +1,14 @@
+export const GAME_CONFIG = {
+  type: Phaser.AUTO,
+  width: 540,
+  height: 540,
+  backgroundColor: "#020817",
+  render: {
+    antialias: true,
+    pixelArt: false,
+  },
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
+};
